6a3d18d08e8fba2ca1d480c5275fb9e6d142851d
Lucas Baizer <62267843+lucasbaizer2@users.noreply.github.com>
2023-02-14 17:49:03 -0800

    Initial commit

---
+++ client/out/extension.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+function activate(_context) {
+}
+exports.activate = activate;
+function deactivate() {
+    return undefined;
+}
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
---
+++ client/src/extension.ts
@@ -0,0 +1,8 @@
+import { ExtensionContext } from 'vscode';
+
+export function activate(_context: ExtensionContext) {
+}
+
+export function deactivate(): Thenable<void> | undefined {
+    return undefined;
+}
