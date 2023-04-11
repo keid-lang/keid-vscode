7b4fd792fb39dcd6483e23bc1b19dc159b56c959
Lucas Baizer <62267843+lucasbaizer2@users.noreply.github.com>
2023-04-11 10:31:22 -0700

    Lots of work

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,8 +1,51 @@
-import { ExtensionContext } from 'vscode';
+import * as path from 'path';
+import { ExtensionContext, StatusBarAlignment, StatusBarItem, window, workspace } from 'vscode';
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind
+} from 'vscode-languageclient/node';
 
-export function activate(_context: ExtensionContext) {
+let client: LanguageClient;
+let serverStatus: StatusBarItem;
+
+export function activate(context: ExtensionContext) {
+    const serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
+    const serverOptions: ServerOptions = {
+        run: {
+            module: serverModule,
+            transport: TransportKind.ipc,
+        },
+        debug: {
+            module: serverModule,
+            transport: TransportKind.ipc,
+        }
+    };
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [{
+            scheme: 'file',
+            language: 'keid'
+        }],
+        synchronize: {
+            fileEvents: [
+                workspace.createFileSystemWatcher('**/*.keid'),
+                workspace.createFileSystemWatcher('**/clust.yaml')
+            ]
+        }
+    };
+
+    serverStatus = window.createStatusBarItem(StatusBarAlignment.Left, 0);
+    serverStatus.text = 'KLS: Starting...';
+    serverStatus.show();
+
+    client = new LanguageClient('kls', 'Keid Language Server', serverOptions, clientOptions);
+    client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-    return undefined;
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
 }
---
+++ server/src/server.ts
@@ -0,0 +1,113 @@
+import {
+	createConnection,
+	TextDocuments,
+	Diagnostic,
+	DiagnosticSeverity,
+	ProposedFeatures,
+	InitializeParams,
+	DidChangeConfigurationNotification,
+	CompletionItem,
+	CompletionItemKind,
+	TextDocumentPositionParams,
+	TextDocumentSyncKind,
+	InitializeResult
+} from 'vscode-languageserver/node';
+
+import {
+	TextDocument
+} from 'vscode-languageserver-textdocument';
+
+const connection = createConnection(ProposedFeatures.all);
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+let hasConfigurationCapability = false;
+let hasWorkspaceFolderCapability = false;
+let hasDiagnosticRelatedInformationCapability = false;
+
+connection.onInitialize((params: InitializeParams) => {
+	const capabilities = params.capabilities;
+
+	hasConfigurationCapability = !!(
+		capabilities.workspace && !!capabilities.workspace.configuration
+	);
+	hasWorkspaceFolderCapability = !!(
+		capabilities.workspace && !!capabilities.workspace.workspaceFolders
+	);
+	hasDiagnosticRelatedInformationCapability = !!(
+		capabilities.textDocument &&
+		capabilities.textDocument.publishDiagnostics &&
+		capabilities.textDocument.publishDiagnostics.relatedInformation
+	);
+
+	const result: InitializeResult = {
+		capabilities: {
+			textDocumentSync: TextDocumentSyncKind.Incremental,
+			completionProvider: {
+				resolveProvider: true
+			}
+		}
+	};
+	if (hasWorkspaceFolderCapability) {
+		result.capabilities.workspace = {
+			workspaceFolders: {
+				supported: true
+			}
+		};
+	}
+	return result;
+});
+
+connection.onInitialized(() => {
+	if (hasConfigurationCapability) {
+		connection.client.register(DidChangeConfigurationNotification.type, undefined);
+	}
+	if (hasWorkspaceFolderCapability) {
+		connection.workspace.onDidChangeWorkspaceFolders(_event => {
+			connection.console.log('Workspace folder change event received.');
+		});
+	}
+});
+
+
+documents.onDidChangeContent(change => {
+	validateTextDocument(change.document);
+});
+
+async function validateTextDocument(textDocument: TextDocument): Promise<void> {
+	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics: [] });
+}
+
+connection.onCompletion(
+	(_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+		return [
+			{
+				label: 'TypeScript',
+				kind: CompletionItemKind.Class,
+				data: 1
+			},
+			{
+				label: 'JavaScript',
+				kind: CompletionItemKind.Interface,
+				data: 2
+			}
+		];
+	}
+);
+
+
+connection.onCompletionResolve(
+	(item: CompletionItem): CompletionItem => {
+		if (item.data === 1) {
+			item.detail = 'TypeScript details';
+			item.documentation = 'TypeScript documentation';
+		} else if (item.data === 2) {
+			item.detail = 'JavaScript details';
+			item.documentation = 'JavaScript documentation';
+		}
+		return item;
+	}
+);
+
+
+documents.listen(connection);
+connection.listen();
